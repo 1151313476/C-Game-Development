ce6708cacccfde9f145e52a9188e7f39fffefd72
mysql <1151313476@qq.com>
2025-08-06 16:34:53 +0800

    .

---
+++ 4、空洞武士/C++/空洞武士/state_node.h
@@ -0,0 +1,18 @@
+#ifndef _STATE_NODE_H_
+#define _STATE_NODE_H_
+
+#include <graphics.h>
+
+class StateNode
+{
+public:
+	StateNode() = default;
+	~StateNode() = default;
+
+	virtual void on_enter() { }
+	virtual void on_update(float delta) { }
+	virtual void on_exit() { }
+
+};
+
+#endif // !_STATE_NODE_H_
